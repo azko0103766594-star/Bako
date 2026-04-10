f6f0acb8a41d68ad30ce12819ed4e7cceaec50bc
azko0103766594-star <azko0103766594@gmail.com>
2026-04-10 09:30:06 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,17 +3,21 @@ export default {
 
     const corsHeaders = {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
     };
 
     if (request.method === "OPTIONS") {
       return new Response(null, { headers: corsHeaders });
     }
 
+    // test
+    if (request.method === "GET") {
+      return new Response("Worker OK 🚀", { headers: corsHeaders });
+    }
+
     if (request.method === "POST") {
       try {
-
         const formData = await request.formData();
         const file = formData.get("file");
 
@@ -23,26 +27,29 @@ export default {
 
         const fileName = `${Date.now()}-${file.name}`;
 
+        // 📦 upload R2
         await env.BUCKET.put(fileName, file.stream(), {
           httpMetadata: {
-            contentType: file.type
+            contentType: file.type || "image/jpeg"
           }
         });
 
+        // 🔥 URL CORRECTE R2 (S3 PUBLIC STYLE)
+        const url = `https://debb6192ab34a20e599bf2d0ab956e6e.r2.cloudflarestorage.com/bako-bako/${fileName}`;
+
         return Response.json({
-          url: `https://debb6192ab34a20e599bf2d0ab956e6e.r2.cloudflarestorage.com/bako-bako/${fileName}`
-        }, {
-          headers: corsHeaders
-        });
+          success: true,
+          url
+        }, { headers: corsHeaders });
 
       } catch (err) {
-        return new Response("ERROR: " + err.message, {
+        return new Response("Error: " + err.message, {
           status: 500,
           headers: corsHeaders
         });
       }
     }
 
-    return new Response("OK");
+    return new Response("Method not allowed", { status: 405, headers: corsHeaders });
   }
 };
