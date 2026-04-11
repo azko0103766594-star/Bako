e22d0f73c034a5cd74cf7ee3a92289626356efa7
azko0103766594-star <azko0103766594@gmail.com>
2026-04-11 23:24:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+https://cool-forest-3f3e.jdjdurirjrrj2.workers.dev/posts
 const feed = document.getElementById("feed");
 const upload = document.getElementById("upload");
 const commentOverlay = document.getElementById("commentOverlay");
