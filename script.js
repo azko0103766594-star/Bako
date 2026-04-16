215116292a0dd1a8e5c9ca4596adb7572faeb6af
azko0103766594-star <azko0103766594@gmail.com>
2026-04-16 19:43:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-function openGame(game) {
-  if (game === "click") {
-    alert("Click Speed bientôt disponible !");
-  }
+let score = 0;
+let time = 10;
+let timer = null;
+let playing = false;
 
-  if (game === "reaction") {
-    alert("Reaction Time bientôt disponible !");
-  }
+let best = localStorage.getItem("bestClick") || 0;
+document.getElementById("best").textContent = best;
 
-  if (game === "memory") {
-    alert("Memory Tap bientôt disponible !");
-  }
+function startGame() {
+  if (playing) return;
+
+  playing = true;
+  score = 0;
+  time = 10;
+
+  document.getElementById("score").textContent = score;
+  document.getElementById("time").textContent = time;
+
+  timer = setInterval(() => {
+    time--;
+    document.getElementById("time").textContent = time;
+
+    if (time === 0) {
+      clearInterval(timer);
+      playing = false;
+
+      if (score > best) {
+        best = score;
+        localStorage.setItem("bestClick", best);
+        document.getElementById("best").textContent = best;
+      }
+
+      alert("Temps terminé ! Score : " + score);
+    }
+  }, 1000);
+}
+
+function addClick() {
+  if (!playing) return;
+  score++;
+  document.getElementById("score").textContent = score;
+}
+
+function resetGame() {
+  clearInterval(timer);
+  playing = false;
+  score = 0;
+  time = 10;
+
+  document.getElementById("score").textContent = 0;
+  document.getElementById("time").textContent = 10;
 }
