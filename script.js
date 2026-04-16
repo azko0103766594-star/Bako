df172d033df2f5f40dd46628f5f2d3d659a15bdb
azko0103766594-star <azko0103766594@gmail.com>
2026-04-16 19:35:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function openGame(game) {
+  if (game === "click") {
+    alert("Click Speed bientôt disponible !");
+  }
 
+  if (game === "reaction") {
+    alert("Reaction Time bientôt disponible !");
+  }
+
+  if (game === "memory") {
+    alert("Memory Tap bientôt disponible !");
+  }
+}
