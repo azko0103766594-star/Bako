f883e7e44377541ac973bd015a65678cd522990a
azko0103766594-star <azko0103766594@gmail.com>
2026-04-20 23:43:54 +0000

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,5 @@
-
+// Animation navbar au scroll
+window.addEventListener("scroll", function(){
+    const nav = document.querySelector(".navbar");
+    nav.classList.toggle("scrolled", window.scrollY > 50);
+});
