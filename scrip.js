022f898eead5980c427f8f00c0dbc05f7e860cfa
azko0103766594-star <azko0103766594@gmail.com>
2026-04-20 23:18:08 +0000

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,13 +1,1 @@
-// Animation apparition des sections
-const sections = document.querySelectorAll("section");
 
-window.addEventListener("scroll", () => {
-  sections.forEach(sec => {
-    const top = window.scrollY;
-    const offset = sec.offsetTop - 400;
-    if(top > offset){
-      sec.style.opacity = "1";
-      sec.style.transform = "translateY(0)";
-    }
-  });
-});
