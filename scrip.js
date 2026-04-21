502de9c43df0bbdab98bd455ada6080fa94b3bce
azko0103766594-star <azko0103766594@gmail.com>
2026-04-21 00:37:26 +0000

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,31 +1,15 @@
-// MENU MOBILE
-const btn = document.getElementById("menuBtn");
-const nav = document.getElementById("nav");
-
-btn.onclick = () => {
-nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-};
-
-// SLIDER HERO
-let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-function showSlide() {
-slides.forEach(s => s.classList.remove("active"));
-slides[index].classList.add("active");
-
-index++;
-if(index >= slides.length) index = 0;
+// WhatsApp réservation
+function book(){
+  window.open("https://wa.me/225000000000", "_blank");
 }
 
-setInterval(showSlide, 3000);
+// Lightbox open
+function openImg(src){
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightboxImg").src = src;
+}
 
-// FORM WHATSAPP
-document.getElementById("form").addEventListener("submit", function(e){
-e.preventDefault();
-
-let text = "Bonjour je veux réserver une chambre";
-let url = "https://wa.me/225000000000?text=" + encodeURIComponent(text);
-
-window.open(url, "_blank");
-});
+// Close lightbox
+function closeImg(){
+  document.getElementById("lightbox").style.display = "none";
+}
