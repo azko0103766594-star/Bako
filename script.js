517d3905f9cec55b598e934d019e2d6d16e305f8
azko0103766594-star <azko0103766594@gmail.com>
2026-06-10 11:07:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 let money = 0;
+let income = 0;
 
 const moneyText = document.getElementById("money");
-const progressFill = document.getElementById("progressFill");
-const unlockShop = document.getElementById("unlockShop");
+const incomeText = document.getElementById("income");
 
-function work(amount){
+document.getElementById("tapBtn").addEventListener("click", () => {
+    money += 1;
+    update();
+});
 
-    money += amount;
-
-    moneyText.innerHTML = "💰 " + money + " €";
-
-    let percent = (money / 500) * 100;
-
-    if(percent > 100){
-        percent = 100;
-    }
-
-    progressFill.style.width = percent + "%";
-
-    if(money >= 500){
-        unlockShop.style.display = "block";
+function buyBalai(){
+    if(money >= 50){
+        money -= 50;
+        income += 1;
+        update();
     }
 }
+
+function buyVelo(){
+    if(money >= 500){
+        money -= 500;
+        income += 10;
+        update();
+    }
+}
+
+function update(){
+    moneyText.textContent = money + " €";
+    incomeText.textContent = "Revenu : " + income + "€/s";
+}
+
+setInterval(() => {
+    money += income;
+    update();
+}, 1000);
