5a90253485c260d909655678dace3f5ea6b999eb
azko0103766594-star <azko0103766594@gmail.com>
2026-06-10 18:45:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,47 +3,23 @@ const player = document.getElementById("player");
 const shadow = document.getElementById("shadow");
 
 let playerX = 50;
+let shadowWidth = 100;
 
-document.addEventListener("mousemove",(e)=>{
+document.addEventListener("mousemove", (e) => {
 
-    sun.style.left = e.clientX - 40 + "px";
-    sun.style.top = e.clientY - 40 + "px";
+    sun.style.left = (e.clientX - 40) + "px";
+    sun.style.top = (e.clientY - 40) + "px";
 
-    const distance =
-    Math.abs(e.clientX - playerX);
+    shadowWidth = Math.abs(e.clientX - playerX) * 0.8;
 
-    shadow.style.width =
-    (distance * 0.8) + "px";
-
+    shadow.style.width = shadowWidth + "px";
 });
 
-setInterval(()=>{
+setInterval(() => {
 
     playerX += 2;
 
     player.style.left = playerX + "px";
-
     shadow.style.left = playerX + "px";
 
-    const shadowWidth =
-    parseInt(shadow.style.width) || 0;
-
-    const shadowEnd =
-    playerX + shadowWidth;
-
-    if(playerX > 400 && playerX < 600){
-
-        if(shadowEnd < 600){
-
-            alert("💀 Tu es tombé !");
-            location.reload();
-        }
-    }
-
-    if(playerX > window.innerWidth - 100){
-
-        alert("🏆 Niveau terminé !");
-        location.reload();
-    }
-
-},20);
+}, 20);
