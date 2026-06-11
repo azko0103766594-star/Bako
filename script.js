986821063d0188a3f355cca63effc1b06fd46f41
azko0103766594-star <azko0103766594@gmail.com>
2026-06-11 00:50:35 +0000

    Refactor jump function and add obstacle movement

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let money = 0;
-const goal = 500;
+const p1 = document.getElementById("player1");
+const p2 = document.getElementById("player2");
 
-function work(amount){
+function jump(player){
+    let p = player === 1 ? p1 : p2;
 
-    money += amount;
+    if(!p.classList.contains("jump")){
+        p.classList.add("jump");
 
-    document.getElementById("money").textContent =
-        money + " €";
-
-    document.getElementById("goalText").textContent =
-        money + " / " + goal + " €";
-
-    document.getElementById("bar").style.width =
-        (money / goal * 100) + "%";
+        setTimeout(()=>{
+            p.classList.remove("jump");
+        },500);
+    }
 }
+
+document.addEventListener("keydown",(e)=>{
+    if(e.key === "a") jump(1);
+    if(e.key === "l") jump(2);
+});
+
+let obs1 = document.getElementById("obstacle1");
+let obs2 = document.getElementById("obstacle2");
+
+let pos1 = 700;
+let pos2 = 1000;
+
+setInterval(()=>{
+
+    pos1 -= 8;
+    pos2 -= 10;
+
+    if(pos1 < -30) pos1 = 700;
+    if(pos2 < -30) pos2 = 700;
+
+    obs1.style.left = pos1 + "px";
+    obs2.style.left = pos2 + "px";
+
+},20);
