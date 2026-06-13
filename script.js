63c0da6a14a53c8ef0fbe08bd1b8d5d6ce515e43
azko0103766594-star <azko0103766594@gmail.com>
2026-06-13 11:50:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function drawStadium() {
 
     if (!stadium.complete) return;
 
-    const zoom = 1.0;
+    const zoom = 2.0;
 
     const drawX = canvas.width / 2 - cameraX * zoom;
 const drawY = canvas.height / 2 - cameraY * zoom;
