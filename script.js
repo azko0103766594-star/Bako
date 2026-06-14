be832f4e72d3001d7b9e4af87ae64bc838af052e
azko0103766594-star <azko0103766594@gmail.com>
2026-06-14 11:19:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -107,8 +107,7 @@ function update() {
     }
 
     stamina = Math.max(0, Math.min(100, stamina));
-
-    trackProgress -= speed * 0.003;
+trackProgress -= speed * 0.0015;
     distance += speed * 0.1;
 
 // POSITION DU JOUEUR
