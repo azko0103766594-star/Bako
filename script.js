626214f3e262702460595cb89c5440018f1d5d19
azko0103766594-star <azko0103766594@gmail.com>
2026-06-14 01:40:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function drawStadium() {
     // image pas prête → stop ici MAIS fond reste visible
     if (!stadium.complete || stadium.naturalWidth === 0) return;
 
-    const zoom = 1.2;
+    const zoom = 2.0;
 
     const drawX = canvas.width / 2 - cameraX * zoom;
     const drawY = canvas.height / 2 - cameraY * zoom;
