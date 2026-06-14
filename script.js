9d76460bf3f8ff5eaec1b0dd7bdac6d427815d2b
azko0103766594-star <azko0103766594@gmail.com>
2026-06-14 10:07:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -112,13 +112,13 @@ function update() {
     distance += speed * 0.1;
 
     // POSITION
-    player.worldX = track.cx + Math.cos(trackProgress) * track.rx;
-    player.worldY = track.cy + Math.sin(trackProgress) * track.ry;
+    const startAngle = Math.PI * 0.75;
 
-    // CAMERA ANGLE
-    let angle = Math.atan2(
-        player.worldY - track.cy,
-        player.worldX - track.cx
+player.worldX =
+    track.cx + Math.cos(trackProgress + startAngle) * track.rx;
+
+player.worldY =
+    track.cy + Math.sin(trackProgress + startAngle) * track.ry;
     );
 
     const lap = trackProgress % (Math.PI * 2);
