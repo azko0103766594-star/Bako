ad353c9338dbd36be5343fb302bfc0566230a20e
azko0103766594-star <azko0103766594@gmail.com>
2026-06-16 22:58:57 +0000

    Update gameState.js

--- gameState.js
+++ gameState.js
@@ -35,3 +35,25 @@ let screen = "menu";
 
 let gameMode = null;
 let playerCount = 0;
+
+function createGame(nbPlayers) {
+
+    players = [];
+
+    for (let i = 0; i < nbPlayers; i++) {
+
+        players.push({
+            coins: 0,
+            turns: 15,
+            skip: false
+        });
+
+    }
+
+    currentPlayer = 0;
+    angle = 0;
+    spinning = false;
+    gameOver = false;
+
+    console.log("Partie créée :", nbPlayers);
+}
