e714b99e1fcafe31f924049d5c0d7cd43f4b2c10
azko0103766594-star <azko0103766594@gmail.com>
2026-06-17 23:51:19 +0000

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,6 @@
+console.log("sounds.js chargé");
+
+const soundClick = new Audio("click.mp3");
+const soundSpin = new Audio("spin.mp3");
+const soundCoin = new Audio("coin.mp3");
+const soundVictory = new Audio("victory.mp3");
