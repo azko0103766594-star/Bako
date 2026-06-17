f58f5ce9628a84b0b644bc1877c19cc8faf04a19
azko0103766594-star <azko0103766594@gmail.com>
2026-06-17 22:37:49 +0000

    Update main.js

--- main.js
+++ main.js
@@ -22,10 +22,44 @@ resize();
 canvas.addEventListener("click", (e) => {
 
     const x = e.offsetX;
-    const y = e.offsetY;
+const y = e.offsetY;
 
-    console.log("Click :", x, y);
+console.log("Click :", x, y);
 
+// ======================
+// FIN DE PARTIE
+// ======================
+
+if (gameOver) {
+
+    // REJOUER
+
+    if (
+        x >= canvas.width / 2 - 180 &&
+        x <= canvas.width / 2 + 180 &&
+        y >= canvas.height - 220 &&
+        y <= canvas.height - 130
+    ) {
+
+        createGame(playerCount);
+        return;
+    }
+
+    // MENU
+
+    if (
+        x >= canvas.width / 2 - 180 &&
+        x <= canvas.width / 2 + 180 &&
+        y >= canvas.height - 120 &&
+        y <= canvas.height - 50
+    ) {
+
+        screen = "menu";
+        return;
+    }
+
+    return;
+}
     // ======================
     // MENU PRINCIPAL
     // ======================
