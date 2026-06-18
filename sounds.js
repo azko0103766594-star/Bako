5df5dbb73e7cc8cc7e1efdfccf41fb57864ced96
azko0103766594-star <azko0103766594@gmail.com>
2026-06-18 15:04:37 +0000

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -3,4 +3,5 @@ console.log("sounds.js chargé");
 const soundClick = new Audio("click.mp3");
 const soundSpin = new Audio("spin.mp3");
 const soundCoin = new Audio("coin.mp3");
+const soundBomb = new Audio("bomb.mp3");
 const soundVictory = new Audio("victory.mp3");
