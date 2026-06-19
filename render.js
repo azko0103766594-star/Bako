466650919f3eae6756e2e2efc187adca1486e78e
azko0103766594-star <azko0103766594@gmail.com>
2026-06-19 13:55:29 +0000

    Update render.js

--- render.js
+++ render.js
@@ -241,7 +241,7 @@ if (board.complete) {
     ctx.drawImage(
         board,
         (canvas.width - boardWidth) / 2,
-        +20,
+        cameraY,
         boardWidth,
         boardHeight
     );
