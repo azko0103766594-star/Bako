822415beb72a6bc128e4c4f119165e6a4419ee81
azko0103766594-star <azko0103766594@gmail.com>
2026-06-26 11:07:22 +0000

    Update APK download link in main.js

--- main.js
+++ main.js
@@ -112,8 +112,7 @@ if (screen === "menu") {
         y <= 590
     ) {
 
-        window.open("https://bako-rosy.vercel.app/Fortuna.apk");
-
+        window.open("https://bako-rosy.vercel.app/app-release.apk");
         return;
     }
 
