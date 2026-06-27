365fc4512b9b013fae477530e42afa8b31a3f704
azko0103766594-star <azko0103766594@gmail.com>
2026-06-27 21:16:48 +0000

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -74,7 +74,7 @@ function calculateReward() {
     let a =
         angle % (Math.PI * 2);
 
-    a += segment / 2.8;
+    a += segment / 2.9;
 
     const index =
         Math.floor(a / segment) %
