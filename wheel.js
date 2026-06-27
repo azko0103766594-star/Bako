843d8bc5ba809faaf2dff61f630d008bbe5162ff
azko0103766594-star <azko0103766594@gmail.com>
2026-06-27 21:58:09 +0000

    Fix division factor for angle calculation

--- wheel.js
+++ wheel.js
@@ -74,7 +74,7 @@ function calculateReward() {
     let a =
         angle % (Math.PI * 2);
 
-    a += segment / 0.0001;
+    a += segment / 0.001;
 
     const index =
         Math.floor(a / segment) %
