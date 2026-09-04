355697e4754e8dec3945763300202cf0a1e3fb4b
azko0103766594-star <azko0103766594@gmail.com>
2026-09-04 10:49:14 +0000

    Delete cotisations.js

--- cotisations.js
+++
@@ -1,1 +0,0 @@
-function calculate(){let a=+document.querySelector('#a').value,b=+document.querySelector('#b').value;document.querySelector('#result').textContent=`Cotisations : ${(a*b/100).toLocaleString('fr-FR')} FCFA`}
